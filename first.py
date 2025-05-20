b4607bda547b3d5163675408d0e863a927e61c00
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-05-20 19:50:07 +0300

    Got the header and date datas in dict

--- first.py
+++ first.py
@@ -17,6 +17,8 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 events = soup.find_all("a", class_="etk")
 
+# Temporary dictionary to store merged data
+eventDict = {}
 
 for event in events:
     eventHeader = event.find_all("h5")
@@ -26,9 +28,33 @@ for event in events:
     for colAuto in colAutos:
         h3Tags = colAuto.find_all("h3")
         pTags = colAuto.find_all("p")
-
         for h3Tag, pTag in zip(h3Tags, pTags):
             day = h3Tag.text.strip()
             month = pTag.text.strip()
-            theEvent = f"{eventHead} {day} {month}"
-            print(theEvent)
+            date = f"{day} {month}"
+
+            # Control if the date has already readed and append it if it is not.
+            if eventHead in eventDict:
+                if date not in eventDict[eventHead]:
+                    eventDict[eventHead].append(date)
+            
+            else:
+                eventDict[eventHead] = [date]
+
+
+# Build final list end merge temporary datas
+eventDateList = []
+
+for header, dates in eventDict.items():
+    if len(dates) == 2:
+        mergedDate = f"{dates[0]} - {dates[1]}"
+    else:
+        mergedDate = dates[0]
+
+    eventDateList.append({
+        "header": header,
+        "date": mergedDate
+    })
+
+for elements in eventDateList:
+    print(elements)
